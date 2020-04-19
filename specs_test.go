37ad52426a30f0a1f1e3e8c44b966ccb01f3050b
John Wang <johncwang@gmail.com>
2020-04-19 15:34:16 -0700

    add OAS3 tests

---
+++ specs_test.go
@@ -0,0 +1,30 @@
+package apispecs
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/grokify/swaggman/openapi3"
+)
+
+var specTests = []struct {
+	filepath string
+	title    string
+}{
+	// {"specs/digital/engage-digital_openapi3.yaml", "RingCentral Engage Digital API"},
+	{"specs/voice/engage-voice_openapi3.json", "RingCentral Engage Voice API"},
+}
+
+// TestSpecs test reading specs.
+func TestSpecs(t *testing.T) {
+	for _, tt := range specTests {
+		spec, err := openapi3.ReadAndValidateFile(tt.filepath)
+		if err != nil {
+			t.Errorf("openapi3.ReadAndValidateFile('%s') Error [%s]", tt.filepath, err.Error())
+		} else if tt.title != spec.Info.Title {
+			t.Errorf("openapi3.ReadFile('%s', true) Want [%s] Got [%s]", tt.filepath, tt.title, spec.Info.Title)
+		} else {
+			fmt.Printf("SPEC_IS_VALID [%s] TITLE [%s]\n", tt.filepath, spec.Info.Title)
+		}
+	}
+}
